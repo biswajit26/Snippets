14fe565b7845f36c209f61e17685f79a56f99bd0
biswajit26 <biswajit.wms@gmail.com>
2017-02-21 18:49:15 +0530

    Update helpers_snippets.php

--- Codeigniter/helpers_snippets.php
+++ Codeigniter/helpers_snippets.php
@@ -1,1 +1,16 @@
+<?php 
+/* common uses helper for CI */
+$this->load->helper(array('form', 'url', 'security'));
 
+/* All CI helper lists 
+   1. Array
+   2.
+*/
+
+/* 1. 
+   Array helper functions [A] element [B] elements  [C] random_element [D] 
+ */
+$this->load->helper('array');
+
+
+?>
