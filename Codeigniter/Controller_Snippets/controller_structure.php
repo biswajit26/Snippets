21ccc703bbf12a000629292b8d23e0228a699470
biswajit26 <biswajit.wms@gmail.com>
2017-02-21 18:29:07 +0530

    Update controller_structure.php

--- Codeigniter/Controller_Snippets/controller_structure.php
+++ Codeigniter/Controller_Snippets/controller_structure.php
@@ -1,1 +1,21 @@
+<?php 
+/*
+ Description : Structure of CI Controller
+*/
+?>
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+class {controller-name} extends CI_Controller {
+  public function __construct()
+  {
+      parent::__construct();
+      // Your own constructor code
+  }
+	/**
+	 * Index Page for this controller.
+	 */
+	public function index()
+	{
+		// Your code here 
+	}
+}
